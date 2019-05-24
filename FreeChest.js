ced095f43cd851650f10fe776990dabc23f5ca7e
ssanna210 <ssanna210@gmail.com>
2019-05-25 00:15:16 +0900

    Create FreeChest.js

---
+++ FreeChest.js
@@ -0,0 +1,9 @@
+handlers.FreeChestCheck = function (args) {
+    try {
+        
+    }catch(e) {
+        var retObj = {};
+        retObj["errorDetails"] = "Error: " + e;
+        return retObj;
+    }
+}
