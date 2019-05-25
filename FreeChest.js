c7248a163f9f3f7e9ef7d1a40cc80d243300f765
ssanna210 <ssanna210@gmail.com>
2019-05-25 22:30:34 +0900

    Update FreeChest.js

--- FreeChest.js
+++ FreeChest.js
@@ -1,5 +1,53 @@
+var stackLimit = 2;
+var cKey = "FreeChests";
+
 handlers.FreeChestCheck = function (args) {
     try {
+        
+        var items = [];
+        var currentTime = new Date();
+        var waitTime = 240;
+        
+        var chests = [];
+        var rData = server.GetUserReadOnlyData( { PlayFabId: currentPlayerId, Keys: [cKey] } );
+        if(rData.Data.hasOwnProperty(cKey)) {
+            chests = JSON.parse( rData.Data[cKey].Value );
+        }else {
+            for(var i=0; i< stackLimit; i++) {
+                var chest = currentTime;
+                chests.push(chest);
+            }
+        }
+        
+        for(var i=0; i< chests.length; i++) {
+            
+            var unLockDate = new Date( chests[i] );
+            
+            if(currentTime.getTime() >= unLockDate.getTime()) {
+                
+                var pull = server.GrantItemsToUser({ PlayFabId: currentPlayerId, ItemIds: ["bundle_supply"] });
+                items = MakeItemData(pull.ItemGrantResults);
+                
+                unLockDate = new Date();
+                unLockDate.setTime(currentTime.getTime() + (waitTime * 1000 * 60));
+                chests[i] = unLockDate;
+                
+                break;
+                
+            }
+            
+        }
+        
+        var rdReq = {
+            "PlayFabId": currentPlayerId,
+            "Data": {}
+        };
+        rdReq.Data[cKey] = JSON.stringify(chests);
+        var rdR = server.UpdateUserReadOnlyData(rdReq);
+        
+        if(items.length == 0) { throw "result is nothing"; }
+        
+        return items;
         
     }catch(e) {
         var retObj = {};
