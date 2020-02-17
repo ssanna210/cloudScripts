aa84b3696d41e4442cf3ac7b09f03edf8b83370c
ssanna2 <ssanna210@gmail.com>
2020-02-18 00:04:21 +0900

    Update FindAnother.js

--- FindAnother.js
+++ FindAnother.js
@@ -1,19 +1,9 @@
 handlers.FindAnother = function (args, context) {
     try {
+        if(args.cost < 1) throw "cost error";
         var cId = currentPlayerId;
-        var rData = server.GetUserReadOnlyData( { PlayFabId: cId, Keys: [cKey] } );
-        if(!rData.Data.hasOwnProperty(cKey)) throw "rData not found :"+ cKey;
-        chest = JSON.parse(rData.Data[cKey].Value);
-        var cnt = GetChestCnt(cSupID);
-        if(cnt <= 0) throw "1004";
-        var pull = server.UnlockContainerItem( { PlayFabId: cId, ContainerItemId: cSupID } );
-        items = MakeItemData(pull.GrantedItems);
-        chest.uDate = new Date();
-        rdUpdate(cKey,chest);
-        if(items.length == 0) throw "1005";
-        chest.items = items; chest.cnt = cnt-1;
-        return chest;
-    }catch(e) {
-        var r = {}; r["errorDetails"] = "Error: " + e; return r;
-    }
+        var inv = server.GetUserInventory({PlayFabId:cId});
+        if(inv.VirtualCurrency.GO < args.cost) throw "lack gold";
+        return server.SubtractUserVirtualCurrency({ PlayFabId: cId, Amount: args.cost, VirtualCurrency: "GO" });
+    }catch(e) { var r = {}; r["errorDetails"] = "Error: " + e; return r; }
 }
