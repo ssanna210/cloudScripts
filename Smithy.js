12478a73bba7af6045d921ce4d71c3bbc322a4dc
ssanna2 <ssanna210@gmail.com>
2020-03-02 10:06:59 +0900

    Update Smithy.js

--- Smithy.js
+++ Smithy.js
@@ -1,5 +1,4 @@
-// args.slotID, args.itemIds, state: ING, FAIL, LOCK, NONE, READY
-handlers.ItemUpgradeStart = function (args) {
+handlers.ItemUpgradeStart = function (args) { // args.slotID, args.itemIds, state: ING, FAIL, LOCK, NONE, READY
     try {
         var items = [];
         items = GetItemData(args.itemIds);
@@ -28,29 +27,22 @@ handlers.ItemUpgradeStart = function (args) {
         return server.UpdateUserReadOnlyData( {  PlayFabId: currentPlayerId, Data : upD, Permission : "Public" } );;
         
     } catch(e) {
-        var retObj = {};
-        retObj["errorDetails"] = "Error: " + e;
-        return retObj;
+        var r = {}; r["errorDetails"] = "Error: " + e; return r;
     }
 }
-
-// args.slotID
-handlers.ItemUpgradeFinish = function (args) {
+handlers.ItemUpgradeFinish = function (args) { // args.slotID
     try {
         var cId = currentPlayerId;
         var r = {};
         r.isUp = false;
         r.isLackTime = false;
-        
         var userD = server.GetUserReadOnlyData( { PlayFabId: cId, Keys: [args.slotID] } );
         if(!userD.Data.hasOwnProperty(args.slotID)) { throw "slot not found"; }
         var slot = {};
         slot = JSON.parse( userD.Data[args.slotID].Value );
-        
         if(slot.hasOwnProperty("openTime")) {
             var unLockDate = new Date( slot.openTime );
             var currentTime = new Date();
-            
             if(currentTime.getTime() < unLockDate.getTime()) {
                 r.isLackTime = true;
                 return r;
@@ -58,17 +50,13 @@ handlers.ItemUpgradeFinish = function (args) {
         }else {
             throw "slot has not openTime's key";
         }
-        
         var items = [];
         items = GetItemData(slot.itemIds);
         if(items.length == 0) { throw "Item instance not found"; }
-        
         var StcR = server.GetPlayerStatistics({ PlayFabId: cId, StatisticNames: [ "TotalTier" ] });
-
         var tierStc = {};
         tierStc.StatisticName = "TotalTier";
         tierStc.Value = 1;
-        
         if(StcR.Statistics.length > 0) {
             for(var i in StcR.Statistics) {
                 if(StcR.Statistics[i].StatisticName == "TotalTier") 
@@ -79,9 +67,7 @@ handlers.ItemUpgradeFinish = function (args) {
         var totalTier = tierStc.Value;
         var tier = parseInt(totalTier % 100);
         var rebirth = parseInt( totalTier / 100 );
-        
         var tD = server.GetTitleData({ Keys : [ "ItemStatTable", "TierTable", "SkillTable", "General" ] });
-        
         var itemT = JSON.parse( tD.Data["ItemStatTable"] );
         var tierT = JSON.parse( tD.Data["TierTable"] );
         var skillT = JSON.parse( tD.Data["SkillTable"] );
@@ -91,30 +77,20 @@ handlers.ItemUpgradeFinish = function (args) {
         for(var index in generalT.ItemUpgradeSlot) {
             if(generalT.ItemUpgradeSlot[index].ID == args.slotID) { slotData = generalT.ItemUpgradeSlot[index]; }
         }
-        
         var upData = {};
-        
         var rndTry = Math.floor(Math.random() * 100) + 1;
-        
-        if(rndTry > slotData.Rate) { 
-            // upgrade failed
+        if(rndTry > slotData.Rate) { // upgrade failed
             r.items = items;
             slot.state = "FAIL";
             upData[slotData.ID] = JSON.stringify(slot);
-            
             server.UpdateUserReadOnlyData( {  PlayFabId: cId, Data : upData, Permission : "Public" } );;
-            
             return r; 
         } 
-        
-        // upgrade success
-        for(var index in items) {
+        for(var index in items) { // upgrade success
             // set stat
             var skill = {};
             skill = JSON.parse(items[index].CustomData.Skill);
-            
             var itemInfo = JSON.parse( items[index].CustomData.Info );
-            
             if(skill.hasOwnProperty("Lev")) {
                 skill.Lev += slotData.Amount;
                 // limit check
@@ -122,56 +98,41 @@ handlers.ItemUpgradeFinish = function (args) {
                     skill.Lev = skill.Limit;
                 }
             }else {
-                
                 var tableD = {};
                 for(var j in itemT.Equipments) {
                     if(itemT.Equipments[j].ItemID == itemInfo.ItemID) {
                         tableD = CopyObj( itemT.Equipments[j] );
                     }
                 }
-                
                 var EquipArray = [];
                 var EquipListData = {};
-        
                 for(var i = 0; i < tierT.EquipList.length; i++) {
                     if(tierT.EquipList[i].Tier <= totalTier) {
                         EquipArray.push(tierT.EquipList[i]);
                     }
                 }
-                
                 skill = GetRandomSkill( tableD.ItemClass, skillT.SkillInfos, EquipListData, EquipArray );
-                
             }
-            
             items[index].CustomData.Skill = JSON.stringify(skill);
-            
             server.UpdateUserInventoryItemCustomData
             ( {PlayFabId: cId, ItemInstanceId: items[index].ItemInstanceId, Data: items[index].CustomData} );
-            
         }
-        
         slot.state = "NONE";
         slot.itemIds = null;
         slot.openTime = null;
-        
         upData[slotData.ID] = JSON.stringify(slot);
-        
         server.UpdateUserReadOnlyData( {  PlayFabId: cId, Data : upData, Permission : "Public" } );;
-        
         r.isUp = true;
         r.items = items;
         
         return r;
-        
     } catch(e) {
         var retObj = {};
         retObj["errorDetails"] = "Error: " + e;
         return retObj;
     }
 }
-
-// args.isRestore, args.slotID
-handlers.FailedItemRestore = function (args) {
+handlers.FailedItemRestore = function (args) { // args.isRestore, args.slotID
     try {
         if(!args.hasOwnProperty("isRestore")) { throw "isRestore not found"; }
         var cId = currentPlayerId;
@@ -213,11 +174,8 @@ handlers.FailedItemRestore = function (args) {
         slot.openTime = null;
         upData[args.slotID] = JSON.stringify(slot);
         server.UpdateUserReadOnlyData( {  PlayFabId: cId, Data : upData, Permission : "Public" } );;
-        
         return r;
     }catch(e) {
-        var r = {};
-        r["errorDetails"] = "Error: " + e;
-        return r;
+        var r = {}; r["errorDetails"] = "Error: " + e; return r;
     }
 }
